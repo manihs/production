98e785441ca7261a3d042b7d0aeba2c6f5d7540d
manihs <869389536q@gmail.com>
2019-01-12 19:00:57 +0530

    3 commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,124 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#ff9800">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+  <style>
+      
+        *{
+      font-family: 'Raleway', sans-serif;
+  }  
+.__login{
+  height: 88vh;
+  position: relative;
+}
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+.pro, .login_container{
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  min-height:500px;
+  background: linear-gradient(#f19f3f,#f9cb4a,#f9e34a,#ffba6a);
+  border-radius: 5px;
+}
+.pro:after{
+  content: ' ';
+  position: absolute;
+  top: -5px;
+  bottom: -5px;
+  right: -5px;
+  left: -5px;
+  border: 2px solid #FFC107;
+  border-radius: 5px;
+}
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+.login_button{
+  position: relative;
+  width: 100px;
+  padding: 8px;
+  border-radius:7px;
+  background: #ec9f45;
+  margin: 10px auto;
+  color: white;
+  text-align: center;
+  margin-top: 40px;
+}
+.login_button:after{
+  content: ' ';
+  position: absolute;
+  top: -4px;
+  bottom: -4px;
+  right: -4px;
+  left: -4px;
+  border: 1px solid #ef965d;
+  border-radius: 10px;
+}
+.container_logo {
+    margin-top: 17%;
+   text-align: center;
+}
+.input_group{
+  width: 100%;
+  text-align: center;
+  margin-bottom:20px;
+}
+.input_group input{
+  padding:10px; 
+  border-radius: 5px;
+  border:none; 
+  width: 100%;
+  box-sizing: border-box;
+}
+.label_input{
+  color: #5d5252;
+  margin-bottom:4px;
+  font-size: 14px;
+}
+.group{
+  margin: 10px auto;
+  width: 80%;
+}
+
+      
+  </style>
+</head>
+<body>
+    
+    
+    <div class="__login">
+ <div class="pro">
+ </div>
+ <div class="login_container">
+     <div class="container_logo">
+        <img src="http://theflyr.com/logo.png">
+     </div>
+     <form method="POST" action="{{ route('login') }}">
                         @csrf
+     <div class="group">
+       <div class="label_input">Username</div>
+       <div class="input_group">
+         <input type="text" name="email" id="" placeholder="username">
+       </div>
+     </div>  
+   <div class="group">
+       <div class="label_input">Password</div>
+       <div class="input_group">
+         <input type="text" name="password" id="" placeholder="**********">
+       </div>
+     </div> 
+     </form>
+     </div> 
+   <div class="login_button" onclick="document.getElementById('form-id').submit();">Login</div>
+ </div>
+ </div>   
+ 
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+    
+    
+</body>
+</html>
