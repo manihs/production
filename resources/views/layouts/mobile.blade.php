bad22bb311b42eadcf6f9266908e9ecf35bebb95
manihs <869389536q@gmail.com>
2019-01-13 18:57:06 +0530

    13-01-2019 layout profile

--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -7,8 +7,11 @@
   <meta name="theme-color" content="#ff9800">
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
 </head>
 <style>@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
 
 *{
   padding: 0px;
